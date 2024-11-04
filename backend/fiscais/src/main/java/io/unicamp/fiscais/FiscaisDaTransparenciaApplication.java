5fe9681a0f75adde90c23e1d53cc8df57e67295d
Felipe Pires <pires.felipe@ifood.com.br>
2024-11-03 22:52:47 -0300

    Add Spring config

---
+++ backend/fiscais/src/main/java/io/unicamp/fiscais/FiscaisDaTransparenciaApplication.java
@@ -0,0 +1,13 @@
+package io.unicamp.fiscais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiscaisDaTransparenciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiscaisDaTransparenciaApplication.class, args);
+	}
+
+}
