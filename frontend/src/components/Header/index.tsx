3b2ce25b14753582273b5e267e3c83f6b623664b
Celso Gabriel Prado <celsogabrielf@gmail.com>
2024-11-15 19:30:40 -0300

    Criação do component Header

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './header.css';
+
+const Header: React.FC = () => {
+  return <h1 className='header'>Header</h1>;
+}
+
+export default Header;
--- frontend/src/pages/LandingPage/index.tsx
+++ frontend/src/pages/LandingPage/index.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import './landingPage.css';
+import Header from 'src/components/Header';
 
 const LandingPage: React.FC = () => {
-  return <h1>LandingPage</h1>;
+  return (
+    <>
+      <Header />
+      <h1>LandingPage</h1>
+    </>
+  );
 }
 
 export default LandingPage;
