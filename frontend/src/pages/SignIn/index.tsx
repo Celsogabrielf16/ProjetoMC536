9cc178b5306d42ee67ee4f708373e7faf9f33eef
Vitor Lacerda <floco.vitor@gmail.com>
2024-11-16 18:07:39 -0300

    Estilização da página login do projeto

--- frontend/src/pages/SignIn/index.tsx
+++ frontend/src/pages/SignIn/index.tsx
@@ -1,7 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { ReactComponent as ClosedEye } from '../../assets/icons/closedEye.svg';
+import { ReactComponent as OpenEye} from '../../assets/icons/openEye.svg';
+import './signIn.css';
+import Header from 'src/components/Header';
 
-const SignIn: React.FC = () => {
-  return <h1>Login</h1>;
+export function SignIn() {
+    const [showPassword, setShowPassword] = useState(false);
+
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+    };
+
+    
+    return (
+      <> 
+        <Header/>
+        <div className="container">
+            <form>
+                <h2>
+                    <span className="highlight">E</span>ntrar
+                </h2>
+
+                <div className="dadosEntrada">
+                    <input type="email" id="email" autoComplete="off" placeholder="Email" required  />
+                </div>
+                <div className="dadosEntrada">
+                    <input
+                        type={showPassword ? 'text' : 'password'}
+                        id="senha"
+                        placeholder="Senha"
+                        required
+                    />
+
+                    <div className='iconEye' onClick={togglePasswordVisibility}>
+                        {showPassword? <OpenEye/> : <ClosedEye/>}
+                    </div>
+                    
+                </div>
+
+                <button type="submit">Entrar</button>
+            </form>
+
+        </div>
+      </>  
+        
+    );
 }
 
 export default SignIn;
