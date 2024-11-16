2d7f95dfe3978564fe27b4382adbd020372e8404
Celso Gabriel Prado <celsogabrielf@gmail.com>
2024-11-15 21:11:03 -0300

    Estilização da secção banner da page LandingPage

--- frontend/src/pages/LandingPage/index.tsx
+++ frontend/src/pages/LandingPage/index.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
+
 import './landingPage.css';
+import { ReactComponent as DownArrow } from "../../assets/icons/downArrow.svg";
+
 import Header from 'src/components/Header';
+
 
 const LandingPage: React.FC = () => {
   return (
     <>
       <Header />
-      <div className='LandingPageMain'>
-        <h1>LandingPage</h1>
-      </div>
+      <main className='LandingPageMain'>
+        <section className='banner'>
+          <div className='banner__content'>
+            <div className='banner__title'>
+              <h3>Transparência em Obras da Unicamp</h3>
+              <h1>Fi<b className='moneyDetail'>$</b>cais da Tran<b className='moneyDetail'>$</b>parência</h1>
+            </div>
+            <div className='banner__image'></div>
+            <div className='banner__arrow'><DownArrow /></div>
+          </div>
+        </section>
+      </main>
     </>
   );
 }
