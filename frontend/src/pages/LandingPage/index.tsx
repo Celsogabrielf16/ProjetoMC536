d7484d222c7e49939977d506ac80608c5d9257ca
Celso Gabriel Prado <celsogabrielf@gmail.com>
2024-11-15 22:32:36 -0300

    Estilização das secções Informations

--- frontend/src/pages/LandingPage/index.tsx
+++ frontend/src/pages/LandingPage/index.tsx
@@ -21,6 +21,34 @@ const LandingPage: React.FC = () => {
             <div className='banner__arrow'><DownArrow /></div>
           </div>
         </section>
+        <section className='first-information'>
+          <h3 className='information__title'>Ace<b className='moneyDetail'>$$</b>o Fácil a Dados Públicos</h3>
+          <p className='information__description'>Exibir dados de forma clara permite que todos compreendam o andamento de projetos públicos. Na Unicamp, isso fortalece a gestão participativa e promove o diálogo com a sociedade, facilitando que qualquer cidadão fiscalize e contribua para uma gestão eficiente.</p>
+          <div className='first-information__cards'>
+            <div>Card 1</div>
+            <div>Card 2</div>
+            <div>Card 3</div>
+            <div>Card 4</div>
+          </div>
+        </section>
+        <section className='second-information'>
+          <div className='second-information__text'>
+            <h3 className='information__title'>Tran<b className='moneyDetail'>$</b>parência nas In<b className='moneyDetail'>$</b>tituições Públicas</h3>
+            <p className='information__description'>Transparência em instituições como a Unicamp promove confiança e responsabilidade. Ao facilitar o acesso aos dados de obras, qualquer cidadão pode acompanhar e fiscalizar o uso de recursos, garantindo que sejam aplicados para o bem da sociedade.</p>
+          </div>
+          <div className='second-information__cards'>
+            Card 1
+          </div>
+        </section>
+        <section className='third-information'>
+          <div className='third-information__cards'>
+            Card 1
+          </div>
+          <div className='third-information__text'>
+            <h3 className='information__title'>Fi<b className='moneyDetail'>$</b>calização Popular: Um Direito</h3>
+            <p className='information__description'>A fiscalização pública de obras é essencial para o uso ético dos recursos. Na Unicamp, facilitar o acesso aos dados de projetos permite que a sociedade acompanhe prazos, custos e resultados, fortalecendo o papel cidadão e promovendo uma gestão transparente e responsável.</p>
+          </div>
+        </section>
       </main>
     </>
   );
