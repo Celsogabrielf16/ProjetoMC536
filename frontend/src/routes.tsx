0f62dc1d8dcdbf6933a289cf4be157a1972cd3bc
Vitor Lacerda <floco.vitor@gmail.com>
2024-11-16 23:20:59 -0300

    Estilização da página de atualização de uma obra do projeto

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -5,6 +5,7 @@ import Home from './pages/Home';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import SubmitProject from './pages/SubmitProject';
+import SubmitUpdate from './pages/SubmitUpdate';
 
 const ROUTES: RouteObject[] = [
   {
@@ -26,6 +27,10 @@ const ROUTES: RouteObject[] = [
   {
     path: '/submit-project',
     Component: SubmitProject,
+  },
+  {
+    path: '/submit-update',
+    Component: SubmitUpdate,
   },
 ];
 
